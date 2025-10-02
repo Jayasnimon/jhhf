52c860783a05e390c6a73402ff4262086b4ff65e
Jayasnimon <103342800+Jayasnimon@users.noreply.github.com>
2025-10-02 23:32:27 +0900

    Create cekid.js

---
+++ cekid.js
@@ -0,0 +1,28 @@
+async function cekId(userId, zoneId) {
+  if (!userId) {
+    throw new Error("Parameter Tidak Valid!");
+    console.log("Tidak Valid");
+  }
+
+  try {
+    const response = await fetch(
+      `https://api-cek-id-game-ten.vercel.app/api/check-id-game?type_name=freefire&userId=${userId}`,
+      {
+        method: "GET",
+        headers: {
+          "User-Agent": "cekid-client"
+        }
+      }
+    );
+
+    if (!response.ok) {
+      throw new Error("Gagal mengambil data, status: " + response.status);
+    }
+
+    const data = await response.json();
+    return data; // kembalikan data JSON
+  } catch (err) {
+    console.error("Error:", err);
+    return { error: true, message: err.message };
+  }
+}
